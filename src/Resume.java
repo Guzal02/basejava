dcffc59da822fd0b5344dfad589b01fc67638712
Guzal <02guzal@gmail.com>
2021-12-25 17:45:53 +0300

    added getter.

--- src/Resume.java
+++ src/Resume.java
@@ -2,9 +2,12 @@
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
     String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
 
     @Override
     public String toString() {
