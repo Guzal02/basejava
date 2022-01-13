326f2bdee699b144c4cca8ebb081145f891232c2
Guzal <02guzal@gmail.com>
2022-01-13 14:43:05 +0300

    Lesson3_abstract storage & sorted storage

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -4,12 +4,16 @@ package ru.javawebinar.basejava.model;
  * ru.javawebinar.basejava.model.Resume class
  */
 
-public class Resume {
+public class Resume implements Comparable<Resume> {
     // Unique identifier
     private String uuid;
 
     public String getUuid() {
         return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 
     @Override
@@ -27,13 +31,14 @@ public class Resume {
         return uuid.hashCode();
     }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
     @Override
     public String toString() {
         return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
 
