455b6cc39adbbac00e08ba59bc939396733daa73
Guzal <02guzal@gmail.com>
2022-02-04 23:51:12 +0300

    Lesson06 Lambda

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -14,12 +14,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
             }
         }
     */
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
-        @Override
-        public int compare(Resume o1, Resume o2) {
-            return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid); // Lambda - java 8
 
     @Override
     protected Integer getSearchKey(String uuid) {
