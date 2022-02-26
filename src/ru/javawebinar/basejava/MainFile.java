05a98c0dd439f8aeb1d52fa31021e8aaca8b5b9d
Guzal <02guzal@gmail.com>
2022-02-26 16:45:49 +0300

    HW09_recursive listing of directories with File.walkFileTree

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -32,6 +32,7 @@ public class MainFile {
             throw new RuntimeException(e);
         }
 
+//   Рекурсивный вывод каталогов и файлов с отступов
 
         Path parent = Paths.get(".\\src\\ru\\javawebinar\\basejava");
         Files.walkFileTree(parent, new SimpleFileVisitor<Path>() {
@@ -46,6 +47,7 @@ public class MainFile {
                 System.out.println(text);
                 return FileVisitResult.CONTINUE;
             }
+
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 int count = file.getNameCount() - parent.getNameCount() + 1;
@@ -59,49 +61,22 @@ public class MainFile {
             }
         });
 
-
 //   Рекурсивный вывод каталогов и файлов без отступов -2 часть
-//        printDirectoryDeeply(dir);
+        printDirectoryDeeply(dir);
+    }
 
-// Рекурсивный вывод каталогов и файлов с отступами -2 часть
-//        if (!dir.exists() || !dir.isDirectory()) {
-//            System.out.println("Incorrect directory name");
-//        }
-//        Deque<Element> stack = new ArrayDeque<>();
-//        stack.add(new Element("-", dir));
-//        while (!stack.isEmpty()) {
-//            Element element = stack.pollLast();
-//            System.out.println(element);
-//            if (element.file.isDirectory()) {
-//                File[] files = element.file.listFiles();
-//                for (int i = Objects.requireNonNull(files).length - 1; i >= 0; i--) {
-//                    stack.add(new Element(element.indent + "--", files[i]));
-//                }
-//            }
-//        }
-//    }
-
-        //   Рекурсивный вывод каталогов и файлов без отступов -1ая часть
-//    public static void printDirectoryDeeply(File dir) {
-//        File[] files = dir.listFiles();
-//        if (files != null) {
-//            for (File file : files) {
-//                if (file.isFile()) {
-//                    System.out.println("File: " + file.getName());
-//                } else if (file.isDirectory()) {
-//                    System.out.println("Directory: " + file.getName());
-//                    printDirectoryDeeply(file);
-//                }
-//            }
-//        }
-//    }
-
-        // Рекурсивный вывод каталогов и файлов с отступами -1ая часть
-//    private record Element(String indent, File file) {
-//
-//        public String toString() {
-//            return indent + file.getName();
-//        }
-//    }
+    //  Рекурсивный вывод каталогов и файлов без отступов -1ая часть
+    public static void printDirectoryDeeply(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println("File: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println("Directory: " + file.getName());
+                    printDirectoryDeeply(file);
+                }
+            }
+        }
     }
 }
