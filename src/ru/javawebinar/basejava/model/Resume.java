3e797014f5736bd1d1a4d9f7dc3028e7690fb95d
Guzal <02guzal@gmail.com>
2022-02-26 23:48:13 +0300

    Lesson10 xml

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.basejava.model;
 
-import java.io.Serial;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.EnumMap;
 import java.util.Map;
@@ -10,16 +12,21 @@ import java.util.UUID;
 /**
  * ru.javawebinar.basejava.model.Resume class
  */
-
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Resume implements Comparable<Resume>, Serializable {
-    @Serial
     private static final long serialVersionUID = 1L;
+
     // Unique identifier
-    private final String uuid;
-    private final String fullName;
+    private String uuid;
+
+    private String fullName;
 
     private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
     private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
+
+    public Resume() {
+    }
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -56,18 +63,16 @@ public class Resume implements Comparable<Resume>, Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Resume resume = (Resume) o;
-
-        if (!uuid.equals(resume.uuid)) return false;
-        return fullName.equals(resume.fullName);
+        return Objects.equals(uuid, resume.uuid) &&
+                Objects.equals(fullName, resume.fullName) &&
+                Objects.equals(contacts, resume.contacts) &&
+                Objects.equals(sections, resume.sections);
     }
 
     @Override
     public int hashCode() {
-        int result = uuid.hashCode();
-        result = 31 * result + fullName.hashCode();
-        return result;
+        return Objects.hash(uuid, fullName, contacts, sections);
     }
 
     @Override
@@ -81,4 +86,3 @@ public class Resume implements Comparable<Resume>, Serializable {
         return cmp != 0 ? cmp : uuid.compareTo(o.uuid);
     }
 }
-
